89d44a67d352f2b240324297cacf410366e2c2e3
João Bosco <joaobosconff03@gmail.com>
2019-11-23 17:32:18 -0300

    create dev

---
+++ src/main/java/br/cesed/unifacisa/bd2/petshop/dao/AnimalDAO.java
@@ -0,0 +1,14 @@
+package br.cesed.unifacisa.bd2.petshop.dao;
+
+import br.cesed.unifacisa.bd2.petshop.beans.Animal;
+
+public class AnimalDAO {
+	
+	
+	public void cadastraAnimal(Animal animal) {
+		String sql = "INSERT INTO EMPREGADO(";
+		String atributos;
+		
+	}
+
+}

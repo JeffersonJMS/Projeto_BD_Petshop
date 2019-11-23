49778cc29d81c4d029e23f95717f085abdb5c57e
João Bosco <joaobosconff03@gmail.com>
2019-11-23 18:51:07 -0300

    implementado logica mind blowing para criacao de ANIMAL

--- src/main/java/br/cesed/unifacisa/bd2/petshop/dao/AnimalDAO.java
+++ src/main/java/br/cesed/unifacisa/bd2/petshop/dao/AnimalDAO.java
@@ -1,14 +1,55 @@
 package br.cesed.unifacisa.bd2.petshop.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import br.cesed.unifacisa.bd2.petshop.beans.Animal;
+import br.cesed.unifacisa.bd2.petshop.connection.Conecta;
 
 public class AnimalDAO {
 	
 	
-	public void cadastraAnimal(Animal animal) {
-		String sql = "INSERT INTO EMPREGADO(";
-		String atributos;
-		
+	public void cadastraAnimal(Animal animal) throws SQLException, ClassNotFoundException{
+		Connection con = Conecta.criarConexao();
+		String sql = "INSERT INTO animal(";
+		String atributos = "registro,raca,tipo,";
+		String valores =" VALUES(" + animal.getRegistro().toString() + "," + "'"+animal.getRaca().toString() +"'" +  "," + "'"+animal.getTipo().toString() +"'"+ ",";
+		if(animal.getPeso() != null) {
+			atributos = atributos + "peso,";
+			valores = valores + animal.getPeso().toString() + ",";
+		}if(animal.getAltura() != null) {
+			atributos = atributos + "altura,";
+			valores = valores + animal.getAltura().toString() + ",";
+		}if(animal.getDtUltMed()!= null) {
+			atributos = atributos + "dtultmed,";
+			valores = valores + animal.getDtUltMed().toString() + ",";
+		}if(animal.getPrecoVenda()!= null) {
+			atributos = atributos + "precovenda,";
+			valores = valores + animal.getPrecoCompra().toString() + ",";
+		}if(animal.getPrecoCompra()!= null) {
+			atributos = atributos + "precocompra,";
+			valores = valores + animal.getPrecoCompra().toString() + ",";
+		}if(animal.getDtNasc()!= null) {
+			atributos = atributos + "dtanasc,";
+			valores = valores + "'"+animal.getDtNasc().toString() + "'" + ",";
+		}
+		atributos = atributos.substring(0, atributos.length() -1);
+		atributos = atributos + ")";
+		//REMOVER ULTIMA VIRGULA
+		valores = valores.substring(0,valores.length() - 1);
+		valores = valores + ")";
+		sql = sql + atributos + valores;
+		try {
+			PreparedStatement preparador = con.prepareStatement(sql);
+			preparador.execute();
+			
+			
+			
+		}catch(SQLException e){
+			System.out.println("Erro" + e.getMessage());
+			
+		}
 	}
 
 }
--- src/main/java/br/cesed/unifacisa/bd2/petshop/testConnection/Teste.java
+++ src/main/java/br/cesed/unifacisa/bd2/petshop/testConnection/Teste.java
@@ -1,20 +1,51 @@
 package br.cesed.unifacisa.bd2.petshop.testConnection;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-
-import br.cesed.unifacisa.bd2.petshop.connection.Conecta;
+import br.cesed.unifacisa.bd2.petshop.beans.Animal;
 
 public class Teste {
 
 	public static void main(String[] args) {
 		
-		try {
-			Connection con = Conecta.criarConexao();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}catch (SQLException e) {
-			e.printStackTrace();
+		Animal animal = new Animal();
+		animal.setRegistro(2);
+		animal.setRaca("arthur");
+		animal.setDtNasc("03/03/1999");
+		animal.setTipo("ROBSON");
+		
+		
+		
+		
+		
+		
+		
+		String sql = "INSERT INTO animal(";
+		String atributos = "registro,raca,tipo,";
+		String valores =" VALUES(" + animal.getRegistro().toString() + "," + "'"+animal.getRaca().toString() +"'" +  "," + "'"+animal.getTipo().toString() +"'"+ ",";
+		if(animal.getPeso() != null) {
+			atributos = atributos + "peso,";
+			valores = valores + animal.getPeso().toString() + ",";
+		}if(animal.getAltura() != null) {
+			atributos = atributos + "altura,";
+			valores = valores + animal.getAltura().toString() + ",";
+		}if(animal.getDtUltMed()!= null) {
+			atributos = atributos + "dtultmed,";
+			valores = valores + animal.getDtUltMed().toString() + ",";
+		}if(animal.getPrecoVenda()!= null) {
+			atributos = atributos + "precovenda,";
+			valores = valores + animal.getPrecoCompra().toString() + ",";
+		}if(animal.getPrecoCompra()!= null) {
+			atributos = atributos + "precocompra,";
+			valores = valores + animal.getPrecoCompra().toString() + ",";
+		}if(animal.getDtNasc()!= null) {
+			atributos = atributos + "dtanasc,";
+			valores = valores + "'"+animal.getDtNasc().toString() + "'" + ",";
 		}
+		atributos = atributos.substring(0, atributos.length() -1);
+		atributos = atributos + ")";
+		//REMOVER ULTIMA VIRGULA
+		valores = valores.substring(0,valores.length() - 1);
+		valores = valores + ")";
+		sql = sql + atributos + valores;
+		System.out.println(sql);
 	}
 }

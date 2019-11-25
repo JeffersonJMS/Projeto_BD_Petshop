bbcb37132043fc7ef7c32df6700012ee0fb84972
João Bosco <joaobosconff03@gmail.com>
2019-11-25 00:29:49 -0300

    feito alguns selects no AnimalDAO

--- src/main/java/br/cesed/unifacisa/bd2/petshop/dao/AnimalDAO.java
+++ src/main/java/br/cesed/unifacisa/bd2/petshop/dao/AnimalDAO.java
@@ -2,15 +2,20 @@ package br.cesed.unifacisa.bd2.petshop.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import br.cesed.unifacisa.bd2.petshop.beans.Animal;
 import br.cesed.unifacisa.bd2.petshop.connection.Conecta;
+//CONFIRMAR SE OS NOMES DOS ATRIBUTOS ESTÃO IGUAIS
 
 public class AnimalDAO {
 	
 	
 	public void cadastraAnimal(Animal animal) throws SQLException, ClassNotFoundException{
+		
 		Connection con = Conecta.criarConexao();
 		String sql = "INSERT INTO animal(";
 		String atributos = "registro,raca,tipo,";
@@ -31,12 +36,11 @@ public class AnimalDAO {
 			atributos = atributos + "precocompra,";
 			valores = valores + animal.getPrecoCompra().toString() + ",";
 		}if(animal.getDtNasc()!= null) {
-			atributos = atributos + "dtanasc,";
+			atributos = atributos + "dtnasc,";
 			valores = valores + "'"+animal.getDtNasc().toString() + "'" + ",";
 		}
 		atributos = atributos.substring(0, atributos.length() -1);
 		atributos = atributos + ")";
-		//REMOVER ULTIMA VIRGULA
 		valores = valores.substring(0,valores.length() - 1);
 		valores = valores + ")";
 		sql = sql + atributos + valores;
@@ -50,6 +54,58 @@ public class AnimalDAO {
 			System.out.println("Erro" + e.getMessage());
 			
 		}
+	}
+	
+	
+	public List<Animal> getTodosAnimais() throws ClassNotFoundException, SQLException{
+		String sql = "SELECT * FROM animal";
+		return getArrayAnimais(sql);
+	}
+	
+	public List<Animal> getAnimaisPorTipo(String tipo) throws ClassNotFoundException, SQLException{
+		String sql = "SELECT * FROM animal where tipo";
+		return getArrayAnimais(sql);
+	}
+	
+	public List<Animal> getAnimaisPorPrecoVenda(Double preco)throws ClassNotFoundException, SQLException{
+		String sql = "SELECT * FROM animal where precovenda <=" + preco;
+		return getArrayAnimais(sql);
+	}
+	
+	
+	private List<Animal> getArrayAnimais(String sql) throws ClassNotFoundException, SQLException{
+		List<Animal> animais = new ArrayList<>();
+		Connection con = Conecta.criarConexao();
+		try {
+			PreparedStatement preparador = con.prepareStatement(sql);
+			ResultSet resultado = preparador.executeQuery();
+			while(resultado.next()) {
+				animais.add(gerarAnimal(resultado));
+			}
+			
+			
+		}catch(SQLException e){
+			System.out.println("Erro" + e.getMessage());
+			
+		}
+		return animais;
+	}
+	
+	
+	
+	
+	private Animal gerarAnimal(ResultSet resultado) throws SQLException {
+		Animal animal = new Animal();
+		animal.setRegistro(resultado.getInt("registro"));
+		animal.setTipo(resultado.getString("tipo"));
+		animal.setPeso(resultado.getDouble("peso"));
+		animal.setAltura(resultado.getDouble("altura"));
+		animal.setDtUltMed(resultado.getString("dtultmed"));
+		animal.setRaca(resultado.getString("raca"));
+		animal.setPrecoCompra(resultado.getDouble("precocompra"));
+		animal.setPrecoVenda(resultado.getDouble("precovenda"));
+		animal.setDtNasc(resultado.getString("dtnasc"));
+		return animal;
 	}
 
 }
